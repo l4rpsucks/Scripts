de8896db0467f66f9c3e20dc55056a1e13f41a35
l4rpsucks <niokisucksatemails@gmail.com>
2025-09-14 17:45:20 -0400

    Update doosmdaydetector.ps1

--- doosmdaydetector.ps1
+++ doosmdaydetector.ps1
@@ -7,5 +7,12 @@ $jarBytes = Invoke-WebRequest -Uri $jarUrl -UseBasicParsing | Select-Object -Exp
 # Define the path to the Java executable
 $javaPath = "C:\Program Files\Java\jre1.8.0_301\bin\java.exe"
 
+# Create a temporary file to hold the JAR content
+$tempFile = [System.IO.Path]::GetTempFileName()
+[System.IO.File]::WriteAllBytes($tempFile, $jarBytes)
+
 # Execute the JAR file using the Java executable
-Start-Process -FilePath $javaPath -ArgumentList "-jar", [System.Text.Encoding]::UTF8.GetString($jarBytes) -NoNewWindow -Wait
+Start-Process -FilePath $javaPath -ArgumentList "-jar", $tempFile -NoNewWindow -Wait
+
+# Clean up the temporary file
+Remove-Item -Path $tempFile
